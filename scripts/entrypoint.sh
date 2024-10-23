60899d8efb04350619be7fbcd8aad736b54e00d2
Alvin Sartor <0xSwego@gmail.com>
2024-10-23 18:01:06 +0100

    fix: connect gateway to dClimate's nodes on startup

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -11,18 +11,23 @@ API_KEY="${API_KEY:?API_KEY is required}"
 echo "Starting IPFS Gateway..."
 ipfs init
 ipfs daemon &
-sleep 3
+sleep 5
 
-echo "IPFS Gateway is ready!"
+echo "Connecting to dClimate's IPFS nodes..."
+ipfs swarm connect "/ip4/15.235.14.184/tcp/4001/p2p/12D3KooWT32PiwLWNhC5nRmhqiJTTJycopo3bxYpCSLr6TCHSn1Q"
+ipfs swarm connect "/ip4/15.235.86.198/tcp/4001/p2p/12D3KooWCKaGcTgY23i2frcH4ZiNF5tcAZ33u4ZNruhkrtpPhd6L"
+ipfs swarm connect "/ip4/148.113.168.50/tcp/4001/p2p/12D3KooWGi5NzUFEsg6gwLwzX2Kvg9QZ88ug1J8sJr6nVhqBC64b"
+
+echo "Connected to dClimate's IPFS nodes!"
 
 ipfs config Swarm.ConnMgr.Type "none"
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"${ALLOW_ORIGINS}\"]"
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods "[\"HEAD\", \"GET\", \"OPTIONS\"]"
 
-ipfs config --json show
+echo "Current IPFS configuration:"
+ipfs config show
 
 echo "Starting go server..."
-
 /app/proxy_app
 
 tail -f /dev/null

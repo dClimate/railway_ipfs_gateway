7c7354b249dded3e9246272ccfa60e3fa6020600
Faolain <Faolain@users.noreply.github.com>
2024-12-19 16:36:35 -0400

    fix: improve reliability between requests

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,9 @@ ipfs config Swarm.ConnMgr.Type "none"
 ipfs daemon & sleep 5
 
 echo "Connecting to dClimate's IPFS nodes..."
-ipfs swarm connect "/ip4/15.235.14.184/udp/4001/quic-v1/p2p/12D3KooWHdZM98wcuyGorE184exFrPEJWv2btXWWSHLQaqwZXuPe"
-ipfs swarm connect "/ip4/15.235.86.198/udp/4001/quic-v1/p2p/12D3KooWGX5HDDjbdiJL2QYf2f7Kjp1Bj6QAXR5vFvLQniTKwoBR"
-ipfs swarm connect "/ip4/148.113.168.50/udp/4001/quic-v1/p2p/12D3KooWPwXW1tXzHoHgMofDwc9uzi7PLVHZt7QbLNt2v3pxzoEF"
+ipfs swarm peering add "/ip4/15.235.14.184/udp/4001/quic-v1/p2p/12D3KooWHdZM98wcuyGorE184exFrPEJWv2btXWWSHLQaqwZXuPe"
+ipfs swarm peering add "/ip4/15.235.86.198/udp/4001/quic-v1/p2p/12D3KooWGX5HDDjbdiJL2QYf2f7Kjp1Bj6QAXR5vFvLQniTKwoBR"
+ipfs swarm peering add "/ip4/148.113.168.50/udp/4001/quic-v1/p2p/12D3KooWPwXW1tXzHoHgMofDwc9uzi7PLVHZt7QbLNt2v3pxzoEF"
 echo "Connected to dClimate's IPFS nodes!"
 
 echo "Current IPFS configuration:"
